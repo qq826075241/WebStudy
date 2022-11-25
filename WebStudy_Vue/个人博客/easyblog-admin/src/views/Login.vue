c106d2b2a9d6b25cff7ff87924331b2408c5ae6f
Cqq <826075241@qq.com>
2022-11-25 17:53:17 +0800

    登录搭建

--- WebStudy_Vue/个人博客/easyblog-admin/src/views/Login.vue
+++ WebStudy_Vue/个人博客/easyblog-admin/src/views/Login.vue
@@ -11,14 +11,19 @@
           <el-input v-model="form.password" placeholder="请输入密码"/>
         </el-form-item>
         <el-form-item label="">
-          <el-input v-model="form.password" placeholder="请输入验证码"/>
-          <img :src="checkCodeUrl" class="check-code">
+          <div class="check-code-panel">
+            <el-input v-model="form.checkCode" placeholder="请输入验证码" class="input-panel"/>
+            <!-- <img :src="checkCodeUrl" class="check-code"> -->
+            <img src="../assets/checkCode.jpeg" alt="" class="check-code"/>
+          </div>
         </el-form-item>
         <el-checkbox-group v-model="form.type">
           <el-checkbox label="记住我" name="type" />
         </el-checkbox-group>
+        <el-form-item label="">
+          <el-button type="primary" :style="{width:'100%'}">登录</el-button>
+        </el-form-item>
       </el-form>
-      <el-button>登录</el-button>
     </div>
   </div>
  
@@ -32,31 +37,50 @@
 
 </script>
 
-<style>
+<style lang="scss">
   .login-body {
     background-image: url(../assets/loginBg.jpeg);
     width: 100%;
     height: calc(100vh);
     background-size: cover;
     background-position: center;
-  }
 
-  .login-panel {
-    width: 350px;
-    float: right;
-    margin-right: 100px;
-    margin-top: 100px;
-    padding: 20px ;
-    border-radius: 20px;
-    box-shadow: 2px 2px 10px #888888;
-    background: #fff;
-  }
+    .login-panel {
+      width: 350px;
+      float: right;
+      margin-right: 100px;
+      margin-top: 100px;
+      padding: 20px ;
+      border-radius: 20px;
+      box-shadow: 2px 2px 10px #888888;
+      background: #fff;
+      opacity: 0.9;
+      
 
-  .login-title {
-    font-size: 18px;
-    text-align: center;
-    color: #333333;
-    margin-bottom: 10px;
+      .login-title {
+      font-size: 18px;
+      text-align: center;
+      color: #333333;
+      margin-bottom: 10px; 
+      }
+
+      .check-code-panel {
+          display: flex;
+          align-items: center;
+          width: 100;
+          
+          .input-panel {
+            flex: 1;
+            margin-right: 25px;
+          }
+          
+          .check-code {
+            object-fit: cover;
+            height: 25px;
+
+          }
+        }
+    }
   }
 
 </style>
